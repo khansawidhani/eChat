03cf09b6f3609259f3eba317a066108cbefc6283
khansawidhani <khansa.ashraf23@gmail.com>
2022-01-05 13:46:48 +0500

    Updated UI

--- lib/ui/modules/home/home.dart
+++ lib/ui/modules/home/home.dart
@@ -1,7 +1,5 @@
-import 'package:echat/app/constants/constants.dart';
 import 'package:echat/controllers/home_controller.dart';
-import 'package:echat/models/user_model.dart';
-import 'package:echat/ui/widgets/drawer/drawer.dart';
+import 'package:echat/ui/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,21 +8,25 @@ class Home extends StatelessWidget {
   final homeController = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
-    // print(currentUser.email);
     return Scaffold(
         drawer: SidePanel(),
-        appBar: AppBar(),
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black87),
+          elevation: 0, 
+          title: Text('eChats App', style: Theme.of(context).textTheme.headline5,), 
+          backgroundColor: Colors.deepPurple[100],
+        ),
         body: SafeArea(
             child: Obx((){
               return homeController.isLoading.isTrue ?
                const Center(child: CircularProgressIndicator(),)
                : ListView.separated(
-              separatorBuilder: (context, index){return Divider(color: Colors.grey.shade300, height: 2.0,);},
+              
               itemBuilder: (context, index){
                 return ListTile(
                   leading: CircleAvatar(child: Text(homeController.users![index].name[0]),),
                   title: Text(homeController.users![index].name.toString()),
-                  trailing: IconButton(icon: const Icon(Icons.arrow_back_ios_new_outlined), onPressed: (){
+                  trailing: IconButton(icon: const Icon(Icons.arrow_back_ios_new_outlined, size: 17,), onPressed: (){
                     Get.toNamed('/chat', arguments: homeController.users![index]);
                   },),
                   onTap: (){
@@ -32,6 +34,9 @@ class Home extends StatelessWidget {
                   },
                 );
               },
+              separatorBuilder: (context, index){
+                return Divider(color: Colors.grey.shade300, height: 2.0,);
+                },
               itemCount: homeController.users!.length
               );
 
