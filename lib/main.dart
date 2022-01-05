6a6886bfc6681da12990d3fc3d220cf42d3a3620
khansawidhani <khansa.ashraf23@gmail.com>
2022-01-05 13:47:12 +0500

    deleted some files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:echat/app/constants/constants.dart';
 import 'package:echat/binding/binding.dart';
-import 'package:echat/binding/home_binding.dart';
+import 'package:echat/constants/constants.dart';
 import 'package:echat/ui/modules/auth/login.dart';
 import 'package:echat/ui/modules/auth/signup.dart';
 import 'package:echat/ui/modules/chat/chat.dart';
@@ -12,45 +11,35 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  Binding().dependencies();
+  InitBinding().dependencies();
   Constants.prefs = await SharedPreferences.getInstance();
   runApp(const Echat());
 }
+
 class Echat extends StatelessWidget {
-  const Echat({ Key? key }) : super(key: key);
+  const Echat({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     bool isUser = Constants.prefs!.getBool('loggedin') ?? false;
-    print(isUser);
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.robotoSlabTextTheme().copyWith(
-          headline4: const TextStyle(color: Colors.black87)
-          
-        )
-      ),
-      title: 'eChat-app',
+          textTheme: GoogleFonts.robotoSlabTextTheme()
+              .copyWith(headline4: const TextStyle(color: Colors.black87))),
+      title: 'eChat',
       initialRoute: isUser ? '/' : '/login',
       getPages: [
-        
-        GetPage(name: '/', page: ()=> Home(), binding: HomeBinding() ), 
-        GetPage(name: '/signup', page: ()=> SignUp()), 
-        GetPage(name: '/login', page: ()=> Login()), 
-        GetPage(name: '/chat', page: ()=> Chat()), 
-
-
-
-
-
+        GetPage(name: '/', page: () => Home(), binding: HomeBinding()),
+        GetPage(name: '/signup', page: () => SignUp()),
+        GetPage(name: '/login', page: () => Login()),
+        GetPage(name: '/chat', page: () => Chat(), binding: ChatBinding()),
       ],
-      
     );
   }
 }
