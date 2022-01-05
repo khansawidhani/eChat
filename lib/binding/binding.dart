0b5b5d0c13faa6ed699d1ad4a4072b78844b8462
khansawidhani <khansa.ashraf23@gmail.com>
2022-01-05 13:41:25 +0500

    created bindings for init dependecy Injection

--- lib/binding/binding.dart
+++ lib/binding/binding.dart
@@ -1,11 +1,25 @@
 import 'package:echat/controllers/auth_controller.dart';
+import 'package:echat/controllers/chat_controller.dart';
+import 'package:echat/controllers/home_controller.dart';
 import 'package:get/get.dart';
 
-class Binding extends Bindings{
+class InitBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(AuthController());
-    
   }
+}
 
+class ChatBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(ChatController());
+  }
+}
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(HomeController());
+  }
 }
