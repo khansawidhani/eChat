9e171a546ccebb4770f818eadc22a8cc3a613b88
khansawidhani <khansa.ashraf23@gmail.com>
2022-01-06 09:59:25 +0500

    Modified factory methods in model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -10,21 +10,14 @@ class UserModel {
     return {'id': id, 'name': name, 'email': email};
   }
 
-  // factory UserModel.fromJson(DocumentSnapshot<Map<String, dynamic>?> json) {
+  static UserModel fromMap(Map<String, dynamic> map) {
+    return UserModel(
 
-  //   final id = json['id'] as String;
-  //   final name = json['name'] as String;
-  //   final email = json['email'] as String;
-
-  //   return UserModel(id: id, email: email, name: name);
-  // }
-
-  // static UserModel fromMap(Map<String, dynamic> map) {
-  //   return UserModel(
-  //     id: map['id'],
-  //     name: map['name'],
-  //     email: map['email'],
-  //   );
-  // }
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      
+    );
+  }
 
 }
